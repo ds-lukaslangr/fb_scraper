df5b27aaef433b3f628dff3cddfc8c0d2e70621c
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-09-17 18:09:46 +0200

    Add build artifact

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+poetry export -f requirements.txt --without-hashes --output requirements.txt
 poetry build
-pex -v $(pwd)/dist/fb_scraper-*-py3-none-any.whl -o fb_scraper.exe -m fb_scraper.main:main
+pex $(pwd)/dist/fb_scraper-*-py3-none-any.whl -r requirements.txt -o fb_scraper.exe -m fb_scraper.main:main

bec6a1b96af6c5a6050fe03d2fe8a769fbf1147c
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2022-09-18 20:01:34 +0200

    Working Excel sheets

--- src/fb_scraper/main.py
+++ src/fb_scraper/main.py
@@ -1,19 +1,27 @@
-import warnings
 import sys
+import warnings
 
-from fb_scraper.scraper import download_posts
+from fb_scraper.scraper import download_posts, base_datetime, write
 
 warnings.filterwarnings("ignore")
 
 
 def main():
     group_ids = sys.argv[1:]
-    print(group_ids)
+    data = {}
+    max_date = base_datetime
 
     for group_id in group_ids:
         print(f"Downloading group={group_id}...")
-        download_posts(group_id)
-        print("Finished")
+        data[group_id], new_max_date = download_posts(group_id)
+        if new_max_date > max_date:
+            max_date = new_max_date
+        print("Download finished.")
+
+    if max_date == base_datetime:
+        print("No new posts.")
+    else:
+        write(data, max_date)
 
 
 if __name__ == "__main__":
--- src/fb_scraper/scraper.py
+++ src/fb_scraper/scraper.py
@@ -1,32 +1,66 @@
 from datetime import datetime
 from pathlib import Path
 
-from facebook_scraper import get_posts
 import pandas as pd
+from facebook_scraper import get_posts
 
 fields_to_extract = [
-    "text",
+    "time",
+    "user_id",
+    "username",
+    "post_url",
     "likes",
+    "shares",
     "reactors",
+    "text",
 ]
+
+date_format = "%Y%m%d_%H%M%S"
+base_datetime = datetime(1, 1, 1)
 
 
 def download_posts(group_id: str):
     data = {field: [] for field in fields_to_extract}
+    max_date = get_max_date()
+    new_max_date = base_datetime
 
-    for post in get_posts(group_id, pages=2):
+    for post in get_posts(group_id, pages=10):
+        if post["time"] < max_date:
+            continue
+
         for field in fields_to_extract:
             data[field].append(post[field])
 
-    date = get_date()
-    path = Path(f"./fb_exports/{date}")
+            if post["time"] > new_max_date:
+                new_max_date = post["time"]
+
+    return data, new_max_date
+
+
+def get_date(datetime_: datetime) -> str:
+    return datetime.strftime(datetime_, date_format)
+
+
+def get_max_date() -> datetime:
+    xlsx_files = Path("./fb_exports").glob("*.xlsx")
+    if not xlsx_files:
+        return base_datetime
+
+    max_date_file = max(xlsx_file for xlsx_file in xlsx_files)
+
+    return datetime.strptime(max_date_file.stem, date_format)
+
+
+def write(data, max_date):
+    date_str = get_date(max_date)
+    path = Path("./fb_exports")
     path.mkdir(parents=True, exist_ok=True)
 
-    df = pd.DataFrame(data)
-    df.to_excel(f"{path.as_posix()}/{group_id}.xlsx", index=False)
-    print(df.head())
-
-
-def get_date() -> str:
-    date_format = "%Y%m%d_%H%M%S"
-    return datetime.strftime(datetime.now(), date_format)
+    # pylint: disable=abstract-class-instantiated
+    with pd.ExcelWriter(f"{path.as_posix()}/{date_str}.xlsx") as writer:
+        for group_id, sheet in data.items():
+            print(f"Writing group={group_id}...")
+            df = pd.DataFrame(sheet)
+            df.to_excel(writer, sheet_name=group_id, index=False)
+            print(df.head())
+            print("Write finished.")
